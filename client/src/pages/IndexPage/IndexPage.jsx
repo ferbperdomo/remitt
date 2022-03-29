e80d7a673e216f88a1d590aa6a9a0dfdc16b1b81
Cristian Perdomo <ferperdomo14@gmail.com>
2022-03-29 11:41:29 +0200

    client created

---
+++ client/src/pages/IndexPage/IndexPage.jsx
@@ -0,0 +1,6 @@
+const IndexPage = () => {
+    return (
+        <h1>Indexpage funcional</h1>
+    )
+}
+export default IndexPage
---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import IndexPage from './../pages/IndexPage/IndexPage'
+
+
+const AppRoutes = () => {
+
+    return (
+        <Routes>
+            <Route path='/' element={<IndexPage />} />
+        </Routes >
+    )
+}
+
+export default AppRoutes
