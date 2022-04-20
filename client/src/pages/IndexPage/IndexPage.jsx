1358597693526c98f4473eecdfaf445a3ed27962
Cristian Perdomo <ferperdomo14@gmail.com>
2022-04-20 17:23:17 +0200

    initial search

--- client/src/pages/IndexPage/IndexPage.jsx
+++ client/src/pages/IndexPage/IndexPage.jsx
@@ -1,8 +1,10 @@
 import apiService from '../../services/api.service'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Container, Form, FormControl, Button, Col, Row } from 'react-bootstrap'
 import '../IndexPage/indexPage.css'
-import countries from "../../countries.json"
+import greenflow from '../../img/greenflow.png'
+import pinkflow from '../../img/pinkflow.png'
+import beigeflow from '../../img/beigeflow.png'
 
 const IndexPage = () => {
     const [input, setInput] = useState({
@@ -11,37 +13,18 @@ const IndexPage = () => {
         sendAmount: ''
     })
 
-    const [sourceCountry, setSourceCountry] = useState(countries)
-    // const [sourceCountrySearch, setSourceCountrySearch] = useState(countries)
     const [source, setSource] = useState()
-
     const { sourceCurrency, targetCurrency, sendAmount } = input
 
-    useEffect(() => {
-        countrySearch()
-    }, [])
-
-    useEffect(() => {
-        sourceCountry && countrySearch()
-    }, [])
 
     const handleInput = e => {
         const { name, value } = e.target
         setInput({ ...input, [name]: value })
-        // setSourceCountry(e.target.value)
-        // console.log('source country', sourceCurrency)
     }
 
     const handleSubmit = e => {
         e.preventDefault()
         oneCall()
-    }
-
-    const countrySearch = sourceCurrency => {
-        const filteredCountries = sourceCountry?.filter(elm => elm.name === sourceCurrency)
-        setSourceCountry(filteredCountries)
-        console.log('país filtrado', filteredCountries)
-        // console.log('hello', sourceCountry[2].name) funciona
     }
 
     const oneCall = () => {
@@ -54,10 +37,19 @@ const IndexPage = () => {
     }
     return (
 
-        <>
-
+        <Container className='mt-5'>
+            <article >
+                <h1>Envío de dinero al exterior</h1>
+                <p>Por favor introduce lo datos</p>
+                <div className='title'>
+                    <img className='greenflow' src={greenflow} alt="testing color" />
+                    <img className='pinkflow' src={pinkflow} alt="testing color" />
+                    <img className='beigeflow' src={beigeflow} alt="testing color" />
+                </div>
+            </article>
+            <hr />
             <Container className="input">
-                <Form className="d-flex mb-3 mt-5" onSubmit={handleSubmit}>
+                <Form className="mb-3 mt-5" onSubmit={handleSubmit}>
                     <FormControl
                         id="sourceCurrency"
                         type="search"
@@ -67,6 +59,7 @@ const IndexPage = () => {
                         value={sourceCurrency}
                         onChange={handleInput}
                     />
+                    <br />
                     <FormControl
                         id="targetCurrency"
                         type="search"
@@ -76,6 +69,7 @@ const IndexPage = () => {
                         value={targetCurrency}
                         onChange={handleInput}
                     />
+                    <br />
                     <FormControl
                         id="sendAmount"
                         type="number"
@@ -84,44 +78,37 @@ const IndexPage = () => {
                         value={sendAmount}
                         onChange={handleInput}
                     />
+                    <br />
                     <Button className="form-button" type="submit">Buscar</Button>
                 </Form >
 
             </Container >
-            <Container>
-                <h1>Envío de dinero al exterior</h1>
-                <p>Moneda enviada: {sourceCurrency}</p>
-                <p>Moneda recibida: {targetCurrency}</p>
-                <p>Cantidad a enviar: {sendAmount}</p>
-                <hr />
-                <h2>Entre las distintas opciones que encontramos, te ofrecemos estas:</h2>
 
-                {
-                    source?.map(provider => {
-                        const { name, type, logos, quotes } = provider
+            <h2>Entre las distintas opciones que encontramos, te ofrecemos estas:</h2>
+            {
+                source?.map(provider => {
+                    const { logos, quotes } = provider
 
-                        return <>
-                            <Row className="justify-content-center text-center">
+                    return <>
+                        <Row className="justify-content-center text-center">
 
-                                <Col >
-                                    <img className='logos' src={logos.normal.pngUrl} />
-                                    <p>{type}: {name}</p>
-                                </Col>
-                                <Col className='mt-3' >
-                                    <p>tipo de cambio: {quotes[0].rate} {targetCurrency}</p>
-                                    <p>comisión: {quotes[0].fee} {sourceCurrency}</p>
-                                    <p>cantidad recibida: {quotes[0].receivedAmount} {targetCurrency}</p>
-                                </Col>
-                            </Row>
+                            <Col >
+                                <img className='logos' src={logos.normal.pngUrl} />
+                                <p>comisión: {quotes[0].fee} {sourceCurrency}</p>
+                            </Col>
+                            <Col className='mt-3' >
+                                <p>tipo de cambio: {quotes[0].rate} {targetCurrency}</p>
+                                <p>cantidad recibida: {quotes[0].receivedAmount} {targetCurrency}</p>
+                            </Col>
+                        </Row>
 
-                            <hr />
-                        </>
-                    })
-                }
+                        <hr />
+                    </>
+                })
+            }
 
-            </Container>
+        </Container>
 
-        </>
     )
 
 }
