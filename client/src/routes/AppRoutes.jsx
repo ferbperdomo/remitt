3b2fd2c309071a1e2e5040d0645a6491a12fa94e
Cristian Perdomo <ferperdomo14@gmail.com>
2022-04-21 15:56:53 +0200

    countries page

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import IndexPage from './../pages/IndexPage/IndexPage'
-
+import CountriesPage from '../pages/CountriesPage/CountriesPage'
 
 const AppRoutes = () => {
 
     return (
         <Routes>
             <Route path='/' element={<IndexPage />} />
+            <Route path='/countries' element={<CountriesPage />} />
         </Routes >
     )
 }
