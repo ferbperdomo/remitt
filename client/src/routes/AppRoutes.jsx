d180dbb0d79e9ce5053aa23295a60ed479417fac
Cristian Perdomo <ferperdomo14@gmail.com>
2022-05-12 19:48:43 +0200

    countries page deleted

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import IndexPage from './../pages/IndexPage/IndexPage'
-import CountriesPage from '../pages/CountriesPage/CountriesPage'
 
 const AppRoutes = () => {
 
     return (
         <Routes>
             <Route path='/' element={<IndexPage />} />
-            <Route path='/countries' element={<CountriesPage />} />
         </Routes >
     )
 }
