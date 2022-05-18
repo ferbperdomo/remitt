5176fbf2d48e3a8222f576e50ef552066e5ea8f5
Cristian Perdomo <ferperdomo14@gmail.com>
2022-05-18 19:04:02 +0200

    index

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -28,8 +28,7 @@ const IndexPage = () => {
     const sendingOptions = countries.map(country => ({
         value: country.currency.code,
         label: country.name.esp,
-        flag: `data:image/png;base64,${country.flag}`
-
+        flag: `data:image/png;base64,${country.flag}`,
     }))
 
     const oneCall = () => {
@@ -38,6 +37,7 @@ const IndexPage = () => {
             .getInput(sourceCurrency, targetCurrency, sendAmount)
             .then(({ data }) => {
                 setSource(data.providers)
+                console.log(data.providers)
             })
             .catch(err => console.log(err))
 
@@ -48,11 +48,13 @@ const IndexPage = () => {
         <div className='main'>
             <section className="text-gray-400 body-font">
                 <div className="container mx-auto flex py-24 md:flex-row flex-col items-center">
+
                     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Hemos hecho lo mejor para ti.</h1>
                         <p className="inline-block title-font sm:text-xl text-2xl mb-4 font-medium text-white" >Atrévete y compara. </p>
                         <p className="mb-8 leading-relaxed">Aquí podrás comparar entre las distintas opciones para el envío de remesas y elegir la opción que más te convenga.</p>
                     </div>
+
                     <div className="lg:max-w-lg lg:w-full md:w-1/2 ">
                         <div className="container px-5 py-15 mx-auto flex">
                             <div className="md:w-full bg-white rounded-lg p-8 flex flex-col  w-full mt-10 md:mt-0 relative z-10 shadow-lg">
@@ -67,7 +69,7 @@ const IndexPage = () => {
                                             onChange={e => setInput({ ...input, sourceCurrency: e.value })}
                                             placeholder="Selecciona el país origen"
                                             name='sourceCurrency'
-                                            className='bg-white  focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 leading-6 transition-colors duration-200 ease-in-out'
+                                            className='bg-white focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 leading-6 transition-colors duration-200 ease-in-out'
                                         />
                                     </div>
 
@@ -80,7 +82,7 @@ const IndexPage = () => {
                                             onChange={e => setInput({ ...input, targetCurrency: e.value })}
                                             placeholder="Selecciona el país destino"
                                             name='targetCurrency'
-                                            className='bg-white  focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 leading-6 transition-colors duration-200 ease-in-out'
+                                            className='bg-white focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 leading-6 transition-colors duration-200 ease-in-out'
                                         />
                                     </div>
 
@@ -122,13 +124,14 @@ const IndexPage = () => {
                     } */}
 
                     {
-                        source === 0 ?
+                        source == 0 ?
                             <div className="flex flex-col text-center w-full mb-10">
                                 <div className="flex flex-col text-center w-full mb-10">
                                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">No hay opciones disponibles</h1>
                                     <p className="mb-8 leading-relaxed" >Por favor, elige otro país.</p>
                                 </div>
                             </div>
+
                             :
 
                             source?.map(provider => {
@@ -140,11 +143,11 @@ const IndexPage = () => {
 
                                         <section className="text-gray-600 body-font relative">
 
-                                            <div className="container px-5 py-5 lg:py-10 mx-auto flex">
+                                            <div className="container py-5 lg:py-10 mx-auto flex">
 
                                                 <div className=" bg-white rounded-lg p-8 flex flex-col md:flex-row w-full mt-10 md:mt-0 relative z-10 shadow-md">
                                                     <div className="xl:w-1/3 md:w-1/2 p-4" key={provider.id}>
-                                                        <div className="w-40 md:w-1/2 h-10 py-2.5 px-5 inline-flex items-center justify-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm  text-center">
+                                                        <div className="w-40 md:w-1/2 h-10 py-2.5 px-5 inline-flex items-center justify-center text-white bg-gradient-to-br from-purple-300 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm  text-center">
                                                             <img className="w-10/12 " viewBox="0 0 24 24" alt='bank logo' src={logos.white.pngUrl} />
                                                         </div>
                                                     </div>
