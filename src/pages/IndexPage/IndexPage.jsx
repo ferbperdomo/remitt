1973013154b0e9fca4b4e1bd5c5a632c9f2078cd
Cristian Perdomo <ferperdomo14@gmail.com>
2022-05-18 19:55:34 +0200

    cards

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -148,18 +148,22 @@ const IndexPage = () => {
                                                 <div className=" bg-white rounded-lg p-8 flex flex-col md:flex-row w-full mt-10 md:mt-0 relative z-10 shadow-md">
                                                     <div className="xl:w-1/3 md:w-1/2 p-4" key={provider.id}>
                                                         <div>
-                                                            <img className="w-10/12 " viewBox="0 0 24 24" alt='bank logo' src={logos.normal.pngUrl} />
+                                                            <img className="w-2/5" viewBox="0 0 24 24" alt='bank logo' src={logos.normal.svgUrl} />
                                                         </div>
+                                                        <br />
+                                                        <p className="w-2/5 px-4 py-1 text-xs text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Comisión: {quotes[0].fee} {sourceCurrency}</p>
                                                     </div>
                                                     <div className="flex flex-col w-full md:w-1/2">
-                                                        <p className="text-sm text-gray-500 mt-3">Comisión: {quotes[0].fee} {sourceCurrency}</p>
-                                                        <p className="text-sm text-gray-500 mt-3">Tipo de cambio: {quotes[0]?.rate.toFixed(2)} {sourceCurrency} </p>
-                                                        <p className="text-sm text-gray-500 mt-3">Cantidad recibida: {quotes[0]?.receivedAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} {targetCurrency} </p>
+
+                                                        <p className="text-sm text-gray-500 mt-3">Tú envías: {sendAmount} {sourceCurrency}</p>
+                                                    </div>
+                                                    <div className="flex flex-col w-full md:w-1/2">
+                                                        <p className="text-sm text-gray-500 mt-3">Allí reciben: {quotes[0]?.receivedAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} {targetCurrency} </p>
                                                     </div>
                                                     <div>
-                                                        <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex">
+                                                        <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex">
                                                             Ir a {provider.name}
-                                                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                                            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                                         </button>
                                                     </div>
                                                 </div>
