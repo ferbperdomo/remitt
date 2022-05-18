35dae60bb027c7c9c1de0ff481ac8d2d5ca7e630
Mikel <mikelvallejo9@gmail.com>
2022-05-18 19:04:59 +0200

    add providers

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -38,6 +38,7 @@ const IndexPage = () => {
             .getInput(sourceCurrency, targetCurrency, sendAmount)
             .then(({ data }) => {
                 setSource(data.providers)
+                console.log(data)
             })
             .catch(err => console.log(err))
 
@@ -144,14 +145,20 @@ const IndexPage = () => {
 
                                                 <div className=" bg-white rounded-lg p-8 flex flex-col md:flex-row w-full mt-10 md:mt-0 relative z-10 shadow-md">
                                                     <div className="xl:w-1/3 md:w-1/2 p-4" key={provider.id}>
-                                                        <div className="w-40 md:w-1/2 h-10 py-2.5 px-5 inline-flex items-center justify-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm  text-center">
-                                                            <img className="w-10/12 " viewBox="0 0 24 24" alt='bank logo' src={logos.white.pngUrl} />
+                                                        <div>
+                                                            <img className="w-2/5" viewBox="0 0 24 24" alt='bank logo' src={logos.normal.svgUrl} />
                                                         </div>
                                                     </div>
                                                     <div className="flex flex-col w-full md:w-1/2">
                                                         <p className="text-sm text-gray-500 mt-3">Comisión: {quotes[0].fee} {sourceCurrency}</p>
                                                         <p className="text-sm text-gray-500 mt-3">Tipo de cambio: {quotes[0]?.rate.toFixed(2)} {sourceCurrency} </p>
                                                         <p className="text-sm text-gray-500 mt-3">Cantidad recibida: {quotes[0]?.receivedAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} {targetCurrency} </p>
+                                                    </div>
+                                                    <div>
+                                                        <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex">
+                                                            Ir a {provider.name}
+                                                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                                        </button>
                                                     </div>
                                                 </div>
                                             </div>
