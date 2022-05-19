216d47688cfa208c3d0217c8b14d6f55640f1af3
Cristian Perdomo <ferperdomo14@gmail.com>
2022-05-19 15:32:03 +0200

    2xl screen

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -20,7 +20,7 @@ export default function Example() {
         <Disclosure as="nav" className="bg-gray-800">
             {({ open }) => (
                 <>
-                    <div className="max-w-7xl mx-auto px-2 sm:px-6 xl:px-0">
+                    <div className="max-w-7xl 2xl:max-w-screen-2xl mx-auto px-2 sm:px-6 xl:px-0">
                         <div className="relative flex items-center justify-between h-16">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
