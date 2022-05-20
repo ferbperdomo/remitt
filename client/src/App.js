a96f7596692ce1926f1267770049500335c44444
Mikel <mikelvallejo9@gmail.com>
2022-05-20 19:33:24 +0200

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import AppRoutes from './routes/AppRoutes'
 import Navigation from './components/Navigation/Navigation'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <AppRoutes />
       </main>
+      <Footer />
     </>
   )
 }
