e84e7d5679aa093a1d53d0ce49418c8911c39b48
Cristian Perdomo <ferperdomo14@gmail.com>
2022-05-20 19:15:40 +0200

    database

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const User = require("../models/User.model")
+const router = express.Router()
+
+
+
+router.post('/signup', (req, res, next) => {
+    
+    const { email, name } = req.body
+
+    if (email === '' || name === '') {
+        res.status(400).json({ message: "Introduce nombre y e-mail ." })
+        return
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/
+    if (!emailRegex.test(email)) {
+        res.status(400).json({ message: 'Introduce un correo electrónico válido.' })
+        return
+    }
+
+    User
+        .findOne({ email })
+        .then((foundUser) => {
+            if (foundUser) {
+                res.status(400).json({ message: "El correo electrónico ya está registrado." })
+                return
+            }
+
+            return User.create({ email, name })
+        })
+        .then((createdUser) => {
+            const { email, name, _id } = createdUser
+
+            const user = { email, name, _id }
+
+            res.status(201).json({ user })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "Internal Server Error" })
+        })
+
+})
+
+module.exports = router
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,8 +1,8 @@
-const router = require("express").Router();
+const router = require("express").Router()
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
+router.use('/auth', require('./auth.routes'))
 
 
-module.exports = router;
+
+
+module.exports = router
