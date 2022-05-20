a371666486c87c42b967235f76a694ddfc6990e8
Mikel <mikelvallejo9@gmail.com>
2022-05-20 20:15:51 +0200

    update providers

--- client/src/pages/IndexPage/IndexPage.jsx
+++ client/src/pages/IndexPage/IndexPage.jsx
@@ -36,8 +36,6 @@ const IndexPage = () => {
     }))
 
 
-
-
     const oneCall = () => {
 
         apiService
@@ -179,7 +177,7 @@ const IndexPage = () => {
                                                     </div>
 
                                                     <div className='mt-6 md:mx-12 md:mt-3 lg:mx-7'>
-                                                        <button type="button" className="rounded-lg text-sm lg:text-lg px-5 py-2.5 text-center flex text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 ">
+                                                        <button type="button" className="rounded-lg text-sm lg:text-lg px-5 py-2.5 text-center flex text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800" >
                                                             Enviar dinero
                                                             <svg className="w-5 h-5 lg:w-10" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                                         </button>
