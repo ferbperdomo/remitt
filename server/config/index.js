b7dca0c90c5fb41574db382a852ccee7a36cdb15
Cristian Perdomo <ferperdomo14@gmail.com>
2022-06-16 14:49:06 +0200

    headers and methods allowed

--- server/config/index.js
+++ server/config/index.js
@@ -16,6 +16,14 @@ module.exports = (app) => {
       }
     )
   )
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "http://localhost:3000")
+    res.header("Access-Control-Allow-Credentials", true)
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
+    next()
+  }
+  )
 
   app.use(logger("dev"))
 
