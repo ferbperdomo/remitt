f986906b8aecbbec71d538eebab35120c7150905
Cristian Perdomo <ferperdomo14@gmail.com>
2022-06-22 18:00:26 +0200

    form colors

--- client/src/pages/IndexPage/IndexPage.jsx
+++ client/src/pages/IndexPage/IndexPage.jsx
@@ -85,7 +85,7 @@ const IndexPage = () => {
                                                 onChange={e => setInput({ ...input, sourceCurrency: e.value })}
                                                 placeholder="Selecciona el país origen"
                                                 name='sourceCurrency'
-                                                className='bg-white focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 leading-6 transition-colors duration-200 ease-in-out'
+                                                className='bg-white focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none leading-6 transition-colors duration-200 ease-in-out'
                                             />
                                         </div>
 
@@ -98,7 +98,7 @@ const IndexPage = () => {
                                                 onChange={e => setInput({ ...input, targetCurrency: e.value })}
                                                 placeholder="Selecciona el país destino"
                                                 name='targetCurrency'
-                                                className='bg-white focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 leading-6 transition-colors duration-200 ease-in-out'
+                                                className='bg-white focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none leading-6 transition-colors duration-200 ease-in-out'
                                             />
                                         </div>
 
@@ -111,7 +111,7 @@ const IndexPage = () => {
                                                 value={sendAmount}
                                                 onChange={handleInput}
 
-                                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 pl-2 leading-8 transition-colors duration-200 ease-in-out" />
+                                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none pl-2 leading-8 transition-colors duration-200 ease-in-out" />
                                         </div>
 
                                         <button type="submit" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full">
